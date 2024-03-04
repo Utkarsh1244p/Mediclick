52c6101f986e835c123906b88e2c792f96b7742b
Utkarsh <utkarshgayguwal123@gmail.com>
2024-03-05 04:55:42 +0530

    All Images Related Nutrition Analysis

---
+++ app/src/main/java/com/example/mediclick/NA2.kt
@@ -0,0 +1,11 @@
+package com.example.mediclick
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NA2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_na2)
+    }
+}
