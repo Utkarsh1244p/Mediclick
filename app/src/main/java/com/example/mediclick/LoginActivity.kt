46dac3e5ad6c6316e61d68f65afb91f27c75639f
Utkarsh <utkarshgayguwal123@gmail.com>
2024-03-08 23:06:56 +0530

    Adding Google and OTP Login

--- app/src/main/java/com/example/mediclick/LoginActivity.kt
+++ app/src/main/java/com/example/mediclick/LoginActivity.kt
@@ -1,15 +1,81 @@
 package com.example.mediclick
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 open class LoginActivity : SignupActivity() {
+
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var googleSignInClient : GoogleSignInClient
+
+    private lateinit var sendOTPBtn : Button
+    private lateinit var phoneNumberET : EditText
+    private lateinit var number : String
+    private lateinit var mProgressBar : ProgressBar
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        auth = FirebaseAuth.getInstance()
+
+        init()
+        sendOTPBtn.setOnClickListener {
+            number = phoneNumberET.text.trim().toString()
+            if (number.isNotEmpty()){
+                if (number.length == 10){
+                    number = "+91$number"
+                    mProgressBar.visibility = View.VISIBLE
+                    val options = PhoneAuthOptions.newBuilder(auth)
+                        .setPhoneNumber(number)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(callbacks) // OnVerificationStateChangedCallbacks
+                        .build()
+                    PhoneAuthProvider.verifyPhoneNumber(options)
+
+                }else{
+                    Toast.makeText(this , "Please Enter correct Number" , Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this , "Please Enter Number" , Toast.LENGTH_SHORT).show()
+
+            }
+        }
+        
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this , gso)
 
         // Assuming you have a button with the ID "myButton" in your activity_page0.xml layout
         val myButton: ImageView = findViewById(R.id.imageView14)
@@ -17,8 +83,130 @@ open class LoginActivity : SignupActivity() {
         // Set onClickListener for the button
         myButton.setOnClickListener {
             // Handle the click event, e.g., start a new activity
-            val intent = Intent(this, FeatureActivity::class.java)
-            startActivity(intent)
+            signInGoogle()
         }
     }
+
+    private fun signInGoogle(){
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if (result.resultCode == Activity.RESULT_OK){
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
+
+    private fun init(){
+        mProgressBar = findViewById(R.id.phoneProgressBar)
+        mProgressBar.visibility = View.INVISIBLE
+        sendOTPBtn = findViewById(R.id.sendOTPBtn)
+        phoneNumberET = findViewById(R.id.phoneEditTextNumber)
+        auth = FirebaseAuth.getInstance()
+    }
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(this , "Authenticate Successfully" , Toast.LENGTH_SHORT).show()
+                    sendToMain()
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    Log.d("TAG", "signInWithPhoneAuthCredential: ${task.exception.toString()}")
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                    }
+                    // Update UI
+                }
+                mProgressBar.visibility = View.INVISIBLE
+            }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUI(account)
+            }
+        }else{
+            Toast.makeText(this, task.exception.toString() , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken , null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful){
+                val intent : Intent = Intent(this , FeatureActivity::class.java)
+                intent.putExtra("email" , account.email)
+                intent.putExtra("name" , account.displayName)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+    private fun sendToMain(){
+        startActivity(Intent(this , MainActivity::class.java))
+    }
+    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            // This callback will be invoked in two situations:
+            // 1 - Instant verification. In some cases the phone number can be instantly
+            //     verified without needing to send or enter a verification code.
+            // 2 - Auto-retrieval. On some devices Google Play services can automatically
+            //     detect the incoming verification SMS and perform verification without
+            //     user action.
+            signInWithPhoneAuthCredential(credential)
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            // This callback is invoked in an invalid request for verification is made,
+            // for instance if the the phone number format is not valid.
+
+            if (e is FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+                Log.d("TAG", "onVerificationFailed: ${e.toString()}")
+            } else if (e is FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+                Log.d("TAG", "onVerificationFailed: ${e.toString()}")
+            }
+            mProgressBar.visibility = View.VISIBLE
+            // Show a message and update the UI
+        }
+
+        override fun onCodeSent(
+            verificationId: String,
+            token: PhoneAuthProvider.ForceResendingToken
+        ) {
+            // The SMS verification code has been sent to the provided phone number, we
+            // now need to ask the user to enter the code and then construct a credential
+            // by combining the code with a verification ID.
+            // Save verification ID and resending token so we can use them later
+            val intent = Intent(this@LoginActivity , OTPActivity::class.java)
+            intent.putExtra("OTP" , verificationId)
+            intent.putExtra("resendToken" , token)
+            intent.putExtra("phoneNumber" , number)
+            startActivity(intent)
+            mProgressBar.visibility = View.INVISIBLE
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        if (auth.currentUser != null){
+            startActivity(Intent(this , MainActivity::class.java))
+        }
+    }
+
+
+
 }
